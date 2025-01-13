42efb209e7f555b29d1fef93892f79339f1171d3
fireleit <fireleit@gmail.com>
2025-01-13 18:18:37 +0400

    feat: add solution to function isContainNumber

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -270,8 +270,13 @@ function getIndexOf(str, letter) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  const str = String(num);
+  const strChecked = String(digit);
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === strChecked) return true;
+  }
+  return false;
 }
 
 /**
